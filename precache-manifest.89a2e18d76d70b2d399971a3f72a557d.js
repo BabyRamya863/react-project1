110cd852eea7d772f03a0d139fdcadeb5817550d
Jayasree-krishna <krishnasarasamma1996@gmail.com>
2020-06-22 23:19:06 +0530

    Updates

---
+++ precache-manifest.89a2e18d76d70b2d399971a3f72a557d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c481c6030f8404a773f76273f913eee2",
+    "url": "/react-project1/index.html"
+  },
+  {
+    "revision": "c26c5e152e4c95bdf676",
+    "url": "/react-project1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b662eca92e261e816fc",
+    "url": "/react-project1/static/js/2.92313060.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project1/static/js/2.92313060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26c5e152e4c95bdf676",
+    "url": "/react-project1/static/js/main.0ad9ce60.chunk.js"
+  },
+  {
+    "revision": "9b9b740693949d18b840",
+    "url": "/react-project1/static/js/runtime-main.c87ccc3a.js"
+  },
+  {
+    "revision": "6f88fc6b60d4b7515f1b0d14efd320a0",
+    "url": "/react-project1/static/media/profileicon.6f88fc6b.svg"
+  }
+]);
